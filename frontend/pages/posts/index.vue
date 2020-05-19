0029de42251a2b5fcdd6dd365f1fd0e6ab9387cd
David Basabe <david.basabe@skyairline.com>
2020-05-19 01:18:28 -0400

    :construction: Adding posts graphql

---
+++ frontend/pages/posts/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Posts page</h2>
+    <form>
+      <input v-model="query" type="search" placeholder="Search something">
+    </form>
+  </div>
+</template>
+
+<script>
+import postsQuery from '~/apollo/queries/post/posts'
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      posts: [],
+      query: ''
+    }
+  },
+  apollo: {
+    posts: {
+      prefetch: true,
+      query: postsQuery
+    }
+  },
+  computed: {
+    filteredPosts () {
+      return this.posts.filter((post) => {
+        return post.title.toLowerCase().includes(this.query.toLowerCase())
+      })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
