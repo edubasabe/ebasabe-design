c57dd77645755be3f3ce434fd9870a07c58b0857
David Basabe <david.basabe@skyairline.com>
2020-05-18 21:23:58 -0400

    :shirt: eslint config done

---
+++ frontend/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <client-only>
+    <nav>
+      <ul class="bg-gray-600">
+        <li class>
+          <router-link tag="a" class="navbar-brand" to="/" exact>
+            Inicio
+          </router-link>
+        </li>
+        <!-- <li><router-link tag="a" class="navbar-brand" to="/restaurants" exact>Restaurants</router-link></li> -->
+      </ul>
+    </nav>
+  </client-only>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style></style>
